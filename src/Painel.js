514cea979ecd852e5eaad4e3f9c3e5a29d0478ce
vagnerGT <70494781+vagnerGT@users.noreply.github.com>
2021-06-25 23:36:50 -0300

    feat(redBlackTree): Add nodeArray prop that allows you to configure the nodes

--- src/Painel.js
+++ src/Painel.js
@@ -4,9 +4,39 @@ import './App.css';
 import RedBlackTree from './RedBlackTree.js';
 
 function Painel() {
+    let test = [
+        {
+            UID: 0,
+            selected: false,
+            value: 50,
+            color: 'red',
+            position: {x: 0, y: 0},
+            parentIndex: null,
+            childIndex: [1, 2],
+        },
+        {
+            UID: 1,
+            selected: false,
+            value: 51,
+            color: 'red',
+            position: {x: 500, y: 800},
+            parentIndex: 0,
+            childIndex: [null, null],
+        },
+        {
+            UID: 2,
+            selected: false,
+            value: 53,
+            color: 'black',
+            position: {x: -100, y: -100},
+            parentIndex: 0,
+            childIndex: [null, null],
+        },
+    ];
+
     return (
         <div className='painel'>
-            <RedBlackTree />
+            <RedBlackTree nodeArray={test} />
         </div>
     );
 }
--- src/RedBlackTree.js
+++ src/RedBlackTree.js
@@ -1,36 +1,63 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 
 import TreeNode from './TreeNode.js';
 
+// prioritize root node on x:0, y:0
+/* nodeData {
+    UID: 0,
+    selected: false,
+    value: 50,
+    color: 'black',
+    position: {x: 0, y: 0},
+    parentIndex: null,
+    childIndex: [null, null],
+}; */
+
 function RedBlackTree(props) {
-    const [state, setState] = useState({
-        pos: {x: 200, y: 200},
-        linePos: {x: 0, y: 0},
-    });
+    function generateNodesAndTreeSize(nodeArray) {
+        if(!nodeArray) return;
+        var pos = {min: {x: null, y: null}, max: {x: null, y: null}};
 
-    useEffect(() => {
-        setTimeout(() => {
-            setState({
-                pos: {x: 200, y: 400},
-                linePos: {x: 300, y: 200},
-            });
-        }, 100);
-    }, []);
+        const nodes = nodeArray.map((item, index, array) => {
+            pos.min.x = Math.min(pos.min.x ?? item.position.x, item.position.x);
+            pos.min.y = Math.min(pos.min.y ?? item.position.y, item.position.y);
+            pos.max.x = Math.max(pos.max.x ?? item.position.x, item.position.x);
+            pos.max.y = Math.max(pos.max.y ?? item.position.y, item.position.y); 
 
-    const pos = state.pos;
-    const linePos = state.linePos;
+            return <TreeNode {...{
+                key: item.UID.toString(),
+                speed: 1,
+                UID: item.UID,
+                selected: item.selected,
+                value: item.value,
+                color: item.color,
+                linePos: array[item.parentIndex]?.position || null,
+                pos: item.position,
+                parentUID: array[item.parentIndex]?.UID || null,
+                childUID: item.childIndex.map(value => array[value]?.UID || null),
+            }} />
+        });
 
-    //<TreeNode value={15} speed={2} linePos={linePos} pos={linePos} color={'Red'} />
+        return [
+            nodes, 
+            {
+                x: (pos.max.x - pos.min.x), 
+                y: (pos.max.y - pos.min.y),
+                minX: (pos.min.x < 0 ? -pos.min.x : 0),
+                minY: (pos.min.y < 0 ? -pos.min.y : 0),
+            },
+        ];
+    }
+
+    const [nodes, treeSize] = generateNodesAndTreeSize(props.nodeArray);
     return (
-        <div className='red-black-tree'>
-            <TreeNode 
-                value={50}
-                speed={1}
-                linePos={{ x: linePos.x, y: linePos.y }}
-                pos={{ x: pos.x, y: pos.y }}
-                color={'Red'}
-            />
+        <div className={'red-black-tree'}>
+            <div style={{width: (treeSize.x + 'px'), height: (treeSize.y + 'px')}}>
+                <div style={{left: treeSize.minX, top: treeSize.minY}}>
+                    {nodes}
+                </div>
+            </div>
         </div>
     );
 }
